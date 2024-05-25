b2019f478729a8f0c877a3e3e02fec0601b0913b
Dave Sargent <dave_sargent@selinc.com>
2024-05-24 17:57:38 -0700

    Finish TODOs

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for micro.
 GH_REPO="https://github.com/zyedidia/micro"
 TOOL_NAME="micro"
 TOOL_TEST="micro --version"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if micro has other means of determining installable versions.
 	list_github_tags
 }
 
